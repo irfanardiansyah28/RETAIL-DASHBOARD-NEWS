8fa9b86d78b9d51f1db0d23d43c27cb17b84fbb7
irfanardiansyah28 <irfanardiansyah598@gmail.com>
2026-05-24 12:11:59 +0700

    restore bootstrap pagination with https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,5 +18,7 @@ class AppServiceProvider extends ServiceProvider
         if (app()->environment('production')) {
             URL::forceScheme('https');
         }
+
+        Paginator::useBootstrap();
     }
 }
