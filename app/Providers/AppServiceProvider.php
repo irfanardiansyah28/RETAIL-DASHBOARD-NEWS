7c1964c295c8e963fa05cbe0362e5e8871ce11e7
irfanardiansyah28 <irfanardiansyah598@gmail.com>
2026-05-24 11:56:11 +0700

    force https production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
-use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-
     public function register(): void
     {
-
         //
     }
 
     public function boot(): void
     {
-
-        Paginator::useBootstrap();
-
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
